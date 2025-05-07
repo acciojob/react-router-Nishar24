b06cdce6047556d24b8b7537798757a8cd528867
Nishar24 <mdnishar75640@gmail.com>
2025-05-07 23:53:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,36 @@
-
 import React from "react";
-import './../styles/App.css';
-import { BrowserRouter,Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./../styles/App.css"; // comment this if the file doesn't exist
 
 const Header = () => {
   return (
     <header>
       <ul>
-       <li> <Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link> </li>
-    </ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </header>
   );
 };
 
-
 const Home = () => {
-  return (
-    <h1>
-     Welcome to my website!
-    </h1>  
-  );
+  return <h1>Welcome to my website!</h1>;
 };
+
 const About = () => {
-  return (
-    <div>
-    <h1>
-     About
-    </h1>
-    <p>This is a sample React Router program.</p>
-    </div>
-  );
+  return <h1>This is a sample React Router program.</h1>;
 };
 
 const App = () => {
   return (
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </BrowserRouter>
-);
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-}
-
-export default App
+export default App;
